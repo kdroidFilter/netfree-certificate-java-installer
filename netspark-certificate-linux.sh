a67f1c16ed412aa113539c4e0ad94b3a5a24e71c
Elie Gambache <elyahou.hadass@gmail.com>
2025-05-15 20:54:42 +0300

    Update netspark-certificate-linux.sh

--- netspark-certificate-linux.sh
+++ netspark-certificate-linux.sh
@@ -13,11 +13,6 @@ trap 'rm -f "$TMP_CERT"' EXIT
 
 curl -fsSL "$CERT_URL" -o "$TMP_CERT"
 
-if ! grep -q "BEGIN CERTIFICATE" "$TMP_CERT"; then
-    echo "Error: invalid certificate." >&2
-    exit 1
-fi
-
 sudo install -d -m 0755 "$CERT_DIR"
 sudo install -m 0644 "$TMP_CERT" "$CERT_DIR/$CERT_NAME"
 
